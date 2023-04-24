dbb4dfa9dca49db5df8fc490f5469bd11b144bd7
said Mansour <mansour50said@gmail.com>
2023-04-24 13:19:56 +0100

    Réalisation du TP2 et debut du TP3

---
+++ tp2CustomerApplicationSaidMansour/src/main/java/iut/dla/tp2customerapplicationsaidmansour/config/ConfigJsf.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iut.dla.tp2customerapplicationsaidmansour.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJsf {
+    
+}
